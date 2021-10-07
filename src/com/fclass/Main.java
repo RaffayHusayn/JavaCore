5de93429eaf291ef5d29bdbbea7ec7815a362da5
Raffay Syed <raffayhusayn@gmail.com>
2021-10-07 05:42:25 -0400

    Second commit made in the master branch just testing

--- src/com/fclass/Main.java
+++ src/com/fclass/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("this is master branch");
+        System.out.println("this is master branch second commit");
     }
 }
