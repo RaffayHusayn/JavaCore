e6da43976fccbc3d0c4976cf8be906d029a619eb
Raffay Syed <raffayhusayn@gmail.com>
2021-10-09 16:46:18 -0400

    Added a stream to double every element of an Int list

--- src/com/fclass/Main.java
+++ src/com/fclass/Main.java
@@ -1,8 +1,15 @@
 package com.fclass;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        List<Integer> marks = Arrays.asList(12,3,44,5);
+        List<Integer> doubledMarks =  marks.stream().map(i -> i * 2).collect(Collectors.toList());
+        System.out.println(doubledMarks);
     }
 
 }
