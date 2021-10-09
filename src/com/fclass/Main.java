550be8b61899b5d53b80e0bb8779256a9c92bd51
Raffay Syed <raffayhusayn@gmail.com>
2021-10-09 18:00:08 -0400

    Removed unneccessary code from main

--- src/com/fclass/Main.java
+++ src/com/fclass/Main.java
@@ -7,9 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Integer> marks = Arrays.asList(12,3,44,5);
-        List<Integer> doubledMarks =  marks.stream().map(i -> i * 2).collect(Collectors.toList());
-        System.out.println(doubledMarks);
+
     }
 
 }
