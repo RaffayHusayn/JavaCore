aabf2737ac2c4f70f9d4db94c8517140ccbea418
Raffay Syed <raffayhusayn@gmail.com>
2021-10-09 18:55:02 -0400

    Added a way to implement threads in java

---
+++ src/com/fclass/MultiThread.java
@@ -0,0 +1,69 @@
+package com.fclass;
+class Name implements Runnable{
+
+
+    public void run(){
+        for(int i=0 ; i<5 ; i++){
+            System.out.println("raffay");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+
+            }
+
+        }
+    }
+
+}
+
+
+class Age implements Runnable {
+
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("24");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+
+            }
+
+        }
+    }
+}
+    public class MultiThread {
+
+        public static void main(String[] args) {
+
+            Name obj1 = new Name();
+            Age obj2 = new Age();
+
+            Thread t1 = new Thread(obj1, "name - thread");
+            Thread t2 = new Thread(obj2, "Age - thread");
+
+            t1.start();
+            try{Thread.sleep(10);}catch(Exception e){
+                System.out.println("something wrong");
+            }
+            t2.start();
+
+
+            try {
+                t1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("both thread are done");
+
+
+        }
+    }
+
