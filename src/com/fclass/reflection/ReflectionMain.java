617edf6cdebc99994679ad74ebddb1e29ee14ed0
Raffay Syed <raffayhusayn@gmail.com>
2021-10-28 17:56:11 -0400

    Use Reflection API to create Object and call functions which are private

--- src/com/fclass/reflection/ReflectionMain.java
+++ src/com/fclass/reflection/ReflectionMain.java
@@ -1,25 +1,26 @@
 package com.fclass.reflection;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.sql.Ref;
+import java.sql.SQLOutput;
 
 public class ReflectionMain {
-    public static void main(String args[]){
-//        Class ref = ReflectionPrivate.class;
-//        String className = ref.getName();
-//        System.out.println(className);
-//
-//        int classMod = ref.getModifiers();
-//        System.out.println(Modifier.isTransient(classMod));
+    public static void main(String args[]) {
+        //Getting class name and access modifier
+        System.out.println("________________Class name and Access Modifier___________________");
         Class ref = ReflectionPrivate.class;
         String classname = ref.getName();
         int modifier = ref.getModifiers();
         System.out.println(modifier);
         System.out.println(classname);
 
-
+        //Doing stuff with Methods of the class
+        System.out.println();
+        System.out.println("___________________Methods of the Class___________________________");
         Method[] classMethods = ref.getMethods();
-        for(Method m: classMethods){
+        for (Method m : classMethods) {
 
             //Getting the names of the method
             System.out.println(m.getName());
@@ -27,13 +28,36 @@ public class ReflectionMain {
             //Getting Access Modifiers of the methods
             int methodModifier = m.getModifiers();
             System.out.println("Is the method Public :   " + Modifier.isPublic(methodModifier));
-            System.out.println("Is the method Private :  "+ Modifier.isPrivate(methodModifier) );
+            System.out.println("Is the method Private :  " + Modifier.isPrivate(methodModifier));
 
             //Getting return type of the methods
             Class returnType = m.getReturnType();
-            System.out.println("The return type is : "+ returnType.getName());
+            System.out.println("The return type is : " + returnType.getName());
+
+            //Getting Parameter of the methods
+            Class[] params = m.getParameterTypes();
+            for (Class p : params) {
+                System.out.println("the param name is : " + p.getName() + "--> " + p.getSimpleName());
+            }
             System.out.println();
+            System.out.println();
+
         }
 
+        //Object Creation with default constructor and param constructor
+        System.out.println("___________________________Object Creation___________________________");
+        try {
+            Constructor<ReflectionPrivate> cons = ReflectionPrivate.class.getConstructor(String.class, int.class);
+            ReflectionPrivate obj = cons.newInstance("raff", 12312);
+
+
+            Class c = Class.forName("com.fclass.reflection.ReflectionPrivate");
+            ReflectionPrivate obj1 = (ReflectionPrivate) c.getDeclaredConstructor().newInstance();
+            Method m1 = c.getDeclaredMethod("toString", null);
+            m1.setAccessible(true);
+            m1.invoke(obj1, null);
+        } catch (Exception e) {
+            System.out.println("something wrong : " + e);
+        }
     }
 }
--- src/com/fclass/reflection/ReflectionPrivate.java
+++ src/com/fclass/reflection/ReflectionPrivate.java
@@ -4,15 +4,21 @@ public class ReflectionPrivate {
     private int number = 12;
     private String name = "raffay";
 
-    public String toString(String n, int no){
-      return n + " " + no;
+    public String toString(){
+        System.out.println("to string : " + this.name + " " + this.number);
+      return this.name + " " + this.number;
     }
     public  void printMessage(String msg){
         System.out.println(msg);
     }
 
+    public ReflectionPrivate(){
+        System.out.println("object created without params");
+    }
     public ReflectionPrivate(String uname, int passcode){
-        System.out.println("method is called with "+ uname + "  "+ passcode);
+        this.number = passcode;
+        this.name = uname;
+        System.out.println("Object is created with "+ uname + "  "+ passcode);
 
     }
 }
