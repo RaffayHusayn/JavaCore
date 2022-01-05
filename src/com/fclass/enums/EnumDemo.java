7148d2204c7b13d93191bf6c2de4165626082680
Raffay Syed <raffayhusayn@gmail.com>
2022-01-05 18:34:28 -0500

    Add fields and constructors in enum

--- src/com/fclass/enums/EnumDemo.java
+++ src/com/fclass/enums/EnumDemo.java
@@ -1,15 +1,27 @@
 package com.fclass.enums;
 
 enum Day{
-    MONDAY,
+    MONDAY("not monday"),
     TUESDAY,
     WEDNESDAY,
     THURSDAY,
     FRIDAY;
+
+    // Creating a field in Enum
+    String favDay;
+
+    // Default Constructor that will be called with all enums if Argument constructor isn't specifically mentioned
+    Day(){
+        this.favDay = "Sunday";
+    }
+    //Argument constructor that will be called with enums that are called with Arguments like MONDAY
+    Day(String favDay){
+        this.favDay = favDay;
+    }
 }
 public class EnumDemo {
     public static void main(String[] args){
-        Day day = Day.MONDAY;
-        System.out.println(day);
+    Day day = Day.TUESDAY;
+        System.out.println(day.favDay);
     }
 }
